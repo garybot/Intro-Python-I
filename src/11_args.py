23a50d7e11a44af74b140ac908abd2cc2476ea24
garybot <argot.pryer@gmail.com>
2020-07-12 10:55:02 -0700

    complete 11-15

--- src/11_args.py
+++ src/11_args.py
@@ -55,7 +55,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -73,4 +75,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
