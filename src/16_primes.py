07ca511be69c8f6eebc762a2ee5b71f0cc944d67
garybot <argot.pryer@gmail.com>
2020-07-13 17:30:01 -0700

    fix boundary conditions bug in sieve

--- src/16_primes.py
+++ src/16_primes.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from functools import reduce
 
 def is_prime(num):
     for i in range(2, num//2 + 1):
@@ -7,14 +8,13 @@ def is_prime(num):
             return False
     return True
 
-if len(sys.argv) == 2:
-    print(is_prime(int(sys.argv[1])))
+# if len(sys.argv) == 2:
+#     print(is_prime(int(sys.argv[1])))
 
 # maybe make a set of all of the numbers up to n,
 # then remove the ones that you know aren't primes,
 # like even numbers above 2, then numbers divisble
 # by 3, 5, 7...
-
 
 def sieve(n):
     """This doesn't work like The Sieve of Eratosthenes but I think it's pretty good. nvm not very good"""
@@ -25,27 +25,28 @@ def sieve(n):
         i = i + 1
     return nums
 
-print(sieve(100))
-
+# print(sieve(100))
 
 def sieve_of_eratosthenes(n):
     """An implementation of The Sieve of Eratosthenes"""
-    nums = [True for x in range(2, n + 1)]
-    for i in range(2, int(math.sqrt(n))):
-        if nums[i] == True:
-            y = [j for j in range(i*i, n, i) if j < len(nums) ]
+    A = [True for x in range(n + 1)]
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if A[i] == True:
+            y = [j for j in range(i*i, n + 1, i) if j < len(A)]
             for j in y:
-                nums[j] = False
-    return nums
+                A[j] = False
+    primes = [i for i in range(2, len(A)) if A[i] == True]
+    return primes
 
-primes = sieve_of_eratosthenes(2000000);
-sum = 0
-for i in range(2, len(primes)):
-    if primes[i] == True:
-        sum += i
-print(sum)
+# print(sieve_of_eratosthenes(100))
+# print(sieve_of_eratosthenes(73))
+primes = sieve_of_eratosthenes(2000000)
+sum = reduce(lambda a, b: a + b, primes)
+# print(sum)
 print(sum == 142913828922) # checking the sum of primes up to 2 million
 
+if len(sys.argv) == 2:
+    print(sieve_of_eratosthenes(int(sys.argv[1])))
 # def primes_from_sets(n):
 #     primes = {x for x in range(2, n + 1)}
 #     for p in primes:
